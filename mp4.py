f547ec68537032e48c2c7168dfd3272fd9dd7bb3
mollinaca <mail@mollinaca.dev>
2025-08-07 22:53:44 +0900

    add

---
+++ mp4.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from yt_dlp import YoutubeDL
+
+def main():
+    print("URL : ", end="")
+    url = input()
+
+    ydl_video_opts = {
+        'format': 'bestvideo+bestaudio/best',  # 柔軟な指定
+        'merge_output_format': 'mp4',
+        'outtmpl': '%(title)s.%(ext)s',  # 動画タイトルに変更
+        'postprocessors': [{
+            'key': 'FFmpegVideoConvertor',
+            'preferedformat': 'mp4',  # 出力を mp4 に
+        }],
+    }
+
+    with YoutubeDL(ydl_video_opts) as ydl:
+        ydl.download([url])
+
+if __name__ == '__main__':
+    main()
